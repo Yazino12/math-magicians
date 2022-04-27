c49bb3c85ebe2a4ef40012f6104f943e7bf1e0b0
yasin <yasin.elsharawi23@gmail.com>
2022-04-27 23:48:14 +0600

    Implement React routing and Add pages Add Home page Add Quote page Use function components

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-import './Calculator.css';
 import calculate from '../logic/calculate';
 
 function Calculator() {
@@ -10,87 +9,90 @@ function Calculator() {
   };
 
   return (
-    <div className="calculator">
-      <header>
-        <p className="result">{state.next || state.total || 0}</p>
-      </header>
-      <section className="buttons">
-        <table>
-          <tbody>
-            <tr>
-              <td onClick={handleClick} role="presentation">
-                AC
-              </td>
-              <td onClick={handleClick} role="presentation">
-                +/-
-              </td>
-              <td onClick={handleClick} role="presentation">
-                %
-              </td>
-              <td className="signs" onClick={handleClick} role="presentation">
-                ÷
-              </td>
-            </tr>
-            <tr>
-              <td onClick={handleClick} role="presentation">
-                7
-              </td>
-              <td onClick={handleClick} role="presentation">
-                8
-              </td>
-              <td onClick={handleClick} role="presentation">
-                9
-              </td>
-              <td className="signs" onClick={handleClick} role="presentation">
-                x
-              </td>
-            </tr>
-            <tr>
-              <td onClick={handleClick} role="presentation">
-                4
-              </td>
-              <td onClick={handleClick} role="presentation">
-                5
-              </td>
-              <td onClick={handleClick} role="presentation">
-                6
-              </td>
-              <td className="signs" onClick={handleClick} role="presentation">
-                -
-              </td>
-            </tr>
-            <tr>
-              <td onClick={handleClick} role="presentation">
-                1
-              </td>
-              <td onClick={handleClick} role="presentation">
-                2
-              </td>
-              <td onClick={handleClick} role="presentation">
-                3
-              </td>
-              <td className="signs" onClick={handleClick} role="presentation">
-                +
-              </td>
-            </tr>
-            <tr>
-              <td
-                className="double-sized"
-                onClick={handleClick}
-                role="presentation"
-              >
-                0
-              </td>
-              <td onClick={handleClick} role="presentation">
-                .
-              </td>
-              <td className="signs" onClick={handleClick} role="presentation">
-                =
-              </td>
-            </tr>
-          </tbody>
-        </table>
-      </section>
+    <div className="calculator-wrapper">
+      <h2>Lets do some Math</h2>
+      <div className="calculator">
+        <header>
+          <p className="result">{state.next || state.total || 0}</p>
+        </header>
+        <section className="buttons">
+          <table>
+            <tbody>
+              <tr>
+                <td onClick={handleClick} role="presentation">
+                  AC
+                </td>
+                <td onClick={handleClick} role="presentation">
+                  +/-
+                </td>
+                <td onClick={handleClick} role="presentation">
+                  %
+                </td>
+                <td className="signs" onClick={handleClick} role="presentation">
+                  ÷
+                </td>
+              </tr>
+              <tr>
+                <td onClick={handleClick} role="presentation">
+                  7
+                </td>
+                <td onClick={handleClick} role="presentation">
+                  8
+                </td>
+                <td onClick={handleClick} role="presentation">
+                  9
+                </td>
+                <td className="signs" onClick={handleClick} role="presentation">
+                  x
+                </td>
+              </tr>
+              <tr>
+                <td onClick={handleClick} role="presentation">
+                  4
+                </td>
+                <td onClick={handleClick} role="presentation">
+                  5
+                </td>
+                <td onClick={handleClick} role="presentation">
+                  6
+                </td>
+                <td className="signs" onClick={handleClick} role="presentation">
+                  -
+                </td>
+              </tr>
+              <tr>
+                <td onClick={handleClick} role="presentation">
+                  1
+                </td>
+                <td onClick={handleClick} role="presentation">
+                  2
+                </td>
+                <td onClick={handleClick} role="presentation">
+                  3
+                </td>
+                <td className="signs" onClick={handleClick} role="presentation">
+                  +
+                </td>
+              </tr>
+              <tr>
+                <td
+                  className="double-sized"
+                  onClick={handleClick}
+                  role="presentation"
+                >
+                  0
+                </td>
+                <td onClick={handleClick} role="presentation">
+                  .
+                </td>
+                <td className="signs" onClick={handleClick} role="presentation">
+                  =
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </section>
+      </div>
     </div>
   );
 }
---
+++ src/components/Home.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <div className="home">
+      <h2>Welcome to our page!</h2>
+      <p>
+        Lorem Ipsum is simply dummy text of the printing and typesetting
+        industry. Lorem Ipsum has been the industrys standard dummy text ever
+        since the 1500s, when an unknown printer took a galley of type and
+        scrambled it to make a type specimen book. It has survived not only five
+        centuries, but also the leap into electronic typesetting, remaining
+        essentially unchanged.
+      </p>
+      <p>
+        Lorem Ipsum is simply dummy text of the printing and typesetting
+        industry. Lorem Ipsum has been the industrys standard dummy text ever
+        since the 1500s, when an unknown printer took a galley of type and
+        scrambled it to make a type specimen book. It has survived not only five
+        centuries, but also the leap into electronic typesetting, remaining
+        essentially unchanged.
+      </p>
+    </div>
+  );
+}
+
+export default Home;
